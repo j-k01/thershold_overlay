7c5b9beceddb3c979bc47e8c5f286c003b246a53
j-k01 <37058255+j-k01@users.noreply.github.com>
2019-05-07 12:03:56 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,18 +2,18 @@ from setuptools import setup, find_packages
 import subprocess
 import sys
 import shutil
-import new_overlay
+import threshold_overlay
 
 setup(
-    name = "new_overlay",
+    name = "threshold_overlay",
     version = new_overlay.__version__,
-    url = 'https://github.com/your_github/new_overlay',
-    license = 'All rights reserved.',
-    author = "Your Name",
-    author_email = "your@email.com",
-    packages = ['new_overlay'],
+    url = 'https://github.com/j-k01/thershold_overlay',
+    license = 'MIT',
+    author = "John Kincaid",
+    author_email = "john.kincaid@ucdenver.com",
+    packages = ['treshold_overlay'],
     package_data = {
     '' : ['*.bit','*.tcl','*.py','*.so'],
     },
-    description = "New custom overlay for PYNQ-Z1"
+    description = "Overlay for PYNQ-Z1 implementing simple image thesholding. Enjoy 2x acceleration! Every cycle can count."
 )
